d9c1613102d04c2b9ac7b24a9ba2ddc47b1a7ad9
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2023-08-18 15:53:44 +0300

    work18082023

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
-const a ="dmytro"
-console.log([...a]);
+function getCommonElements(array1, array2) {
+    // Change code below this line
+  let newArray = [];
+  
+    for (let i=0; i<array1.length; i+=1) {
+      if(array2.includes(array1[i])) {
+        newArray.push(array1[i])
+      }
+    }
+    return newArray
+   // Change code above this line
+  }
+
